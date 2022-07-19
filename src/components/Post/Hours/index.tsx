19160fe604b39b3af84c1ed4389be7938fdb9a70
KevinKangGithub <haochenkang@gmail.com>
2022-07-19 15:10:55 -0600

    Modified hours post body to be table format.

--- src/components/Post/Hours/index.tsx
+++ src/components/Post/Hours/index.tsx
@@ -1,9 +1,11 @@
+import UserProfile from '@components/Shared/UserProfile'
 import Markup from '@components/Shared/Markup'
 import { Card } from '@components/UI/Card'
 import { BCharityPost } from '@generated/bcharitytypes'
 import imagekitURL from '@lib/imagekitURL'
 import React, { FC, ReactNode } from 'react'
 import { STATIC_ASSETS } from 'src/constants'
+import { profile } from 'console'
 
 const Badge: FC<BadgeProps> = ({ title, value }) => (
   <div className="flex bg-gray-200 rounded-full border border-gray-300 dark:bg-gray-800 dark:border-gray-700 text-[12px] w-fit">
@@ -25,7 +27,8 @@ interface Props {
 
 const Hours: FC<Props> = ({ post }) => {
   const cover = post?.metadata?.cover?.original?.url
-
+  const postType = post?.metadata?.attributes[0]?.value
+  
   return (
     <Card forceRounded testId="hours">
       <div
@@ -44,36 +47,62 @@ const Hours: FC<Props> = ({ post }) => {
       />
       <div className="p-5">
         <div className="mr-0 space-y-1 sm:mr-16"></div>
+        
 
         <div className="text-xl font-bold">
           {' '}
-          VHR Submission for {post.metadata.name},{' '}
-          {`${post.metadata.attributes[2].value} `}
+          VHR Verifcation Submitted by {post.profile.name} ({post.profile.handle}):
+        </div>
+        <br></br>
+        <div>
+        <table className="border border-violet-500 w-10 whitespace-nowrap">
+          <tr className="text-center font-bold bg-violet-200">
+            <th className="border border-violet-500 px-6 py-2">Organization</th>
+            <th className="border border-violet-500 px-6 py-2">{post.metadata.name}</th>
+          </tr>
+          <tr className="text-center font-bold bg-violet-200">
+            <th className="border border-violet-500 px-6 py-2">Organization Wallet</th>
+            <th className="border border-violet-500 px-6 py-2">{post.metadata.attributes[1].value?.substring(0, 30) + '...'}</th>
+          </tr>
+          <tr className="text-center font-bold bg-violet-200">
           {!(
-            post.metadata.attributes[2].value ===
-            post.metadata.attributes[3].value
-          ) && `to ${post.metadata.attributes[3].value}`}
+              post.metadata.attributes[2].value ===
+              post.metadata.attributes[3].value
+            ) ?
+            <th className="border border-violet-500 px-6 py-2">Start Date</th>
+            :
+            <th className="border border-violet-500 px-6 py-2">Date</th>
+          }
+            <th className="border border-violet-500 px-6 py-2">{post.metadata.attributes[2].value}</th>
+          </tr>
+          
+          {!(
+              post.metadata.attributes[2].value ===
+              post.metadata.attributes[3].value
+            ) && 
+            <>
+            <tr className="text-center font-bold bg-violet-200">
+              <th className="border border-violet-500 px-6 py-2">End Date</th>
+              <th className="border border-violet-500 px-6 py-2">{post.metadata.attributes[3].value}</th>
+            </tr>
+            </>
+          }
+          <tr className="text-center font-bold bg-violet-200">
+            <th className="border border-violet-500 px-6 py-2">Total Hours</th>
+            <th className="border border-violet-500 px-6 py-2">{post.metadata.attributes[4].value}</th>
+          </tr>
+
+        </table>
         </div>
 
         <br></br>
-
-        <div className="text-sm leading-7 whitespace-pre-wrap break-words">
+        <div className="text-lg leading-2 whitespace-pre-wrap break-words">
           <Markup>{post.metadata.description}</Markup>
         </div>
 
-        <br></br>
-
-        <div className="text-sm leading-7 whitespace-pre-wrap break-words">
-          <Badge
-            title={
-              <div className="flex items-center space-x-1">
-                <div>Total Hours</div>
-              </div>
-            }
-            value={post.metadata.attributes[4].value}
-          />
-        </div>
       </div>
+
+      
 
       <br></br>
     </Card>
