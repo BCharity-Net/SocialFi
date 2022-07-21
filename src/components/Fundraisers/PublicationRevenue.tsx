8096733fb1c41f1ff4c600e4b8005bc1761b4b1d
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2022-07-21 16:58:18 -0600

    New working fundraiser posts
    Fixed fundraisers and added more things

---
+++ src/components/Fundraisers/PublicationRevenue.tsx
@@ -0,0 +1,50 @@
+import { gql, useQuery } from '@apollo/client'
+import Logger from '@lib/logger'
+import { FC } from 'react'
+
+interface Props {
+  fund: any
+  callback?: Function
+}
+
+const PUBLICATION_REVENUE_QUERY = gql`
+  query PublicationRevenue($request: PublicationRevenueQueryRequest!) {
+    publicationRevenue(request: $request) {
+      revenue {
+        total {
+          value
+        }
+      }
+    }
+  }
+`
+const RevenueDetails: FC<Props> = ({ fund, callback }) => {
+  useQuery(PUBLICATION_REVENUE_QUERY, {
+    variables: {
+      request: {
+        publicationId:
+          fund?.__typename === 'Mirror'
+            ? fund?.mirrorOf?.id
+            : fund?.pubId ?? fund?.id
+      }
+    },
+    onCompleted(data) {
+      Logger.log(
+        '[Query]',
+        `Fetched fundraise revenue details Fundraise:${fund?.pubId ?? fund?.id}`
+      )
+      if (!callback) return
+      callback(data?.publicationRevenue?.revenue?.total?.value)
+    }
+  })
+  return <div />
+}
+
+// console.log('revenueData before', revenueData)
+// useEffect(() => {
+//   setRevenue(
+//     parseFloat(revenueData?.publicationRevenue?.revenue?.total?.value ?? 0)
+//   )
+// }, [revenueData])
+// console.log(revenueData?.publicationRevenue?.revenue?.total?.value)
+export default RevenueDetails
