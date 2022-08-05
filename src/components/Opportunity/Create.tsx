849df4e1909733927400b64dc0e5ca7efc14cf12
liraymond04 <li.raymond04@gmail.com>
2022-08-05 09:29:16 -0600

    Updated indexing text for creating Opportunity

--- src/components/Opportunity/Create.tsx
+++ src/components/Opportunity/Create.tsx
@@ -362,9 +362,9 @@ const Opportunity: NextPage = () => {
               txHash={
                 data?.hash ? data?.hash : broadcastData?.broadcast?.txHash
               }
-              indexing="Hour Submission creation in progress, please wait!"
-              indexed="Hour Submission created successfully"
-              type="hours"
+              indexing="Volunteer Opportunity creation in progress, please wait!"
+              indexed="Volunteer Opportunity created successfully"
+              type="opportunity"
               urlPrefix="posts"
             />
           ) : (
